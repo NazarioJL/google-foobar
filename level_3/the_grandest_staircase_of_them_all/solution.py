818390495a1d339619bc983e3ceafd4315027d94
NazarioJL <jlnazario@hotmail.com>
2017-02-16 15:30:14 -0800

    Added solution to 2nd problem in level 3 and associated tests. Need to further improve performance runtime.

--- level_3/the_grandest_staircase_of_them_all/solution.py
+++ level_3/the_grandest_staircase_of_them_all/solution.py
@@ -1,2 +1,53 @@
 def answer(n):
-    # your code here
+    # there will be only *one* sequence with count 1
+    result = make_stairs_count(n) - 1
+    return result
+
+
+def make_stairs(total_remaining):
+    """Returns a list of all sequences of increasing values that add up to total_remaining"""
+    all_lists = []
+
+    def make_stairs_rec(prev_step_size, left, l):
+        if left == 0:
+            all_lists.append(l)
+            return
+        if left < 0:
+            return
+
+        for new_step_size in xrange(prev_step_size + 1, left + 1):
+            new_left = left - new_step_size
+            make_stairs_rec(new_step_size, new_left, l + [new_step_size])
+
+        return
+
+    make_stairs_rec(0, total_remaining, [])
+
+    return all_lists
+
+
+def make_stairs_count(total_remaining):
+    """Returns the count of all sequences of increasing values that add up to total_remaining
+
+    Since the problem only requires the count, this method will not keep track of the
+    actual sequence.
+
+    """
+    def make_stairs_count_rec(prev_step_size, remaining):
+        if remaining == 0:
+            return 1
+        if remaining < 0:
+            return 0
+
+        result = 0
+
+        for new_step_size in xrange(prev_step_size + 1, remaining + 1):
+            new_remaining = remaining - new_step_size
+            result += make_stairs_count_rec(new_step_size, new_remaining)
+            # TODO: further pruning, and consider dynamic memory approach
+
+        return result
+
+    all_count = make_stairs_count_rec(0, total_remaining)
+
+    return all_count
--- level_3/the_grandest_staircase_of_them_all/tests/test_answer.py
+++ level_3/the_grandest_staircase_of_them_all/tests/test_answer.py
@@ -1,7 +1,25 @@
 from solution import answer
 
 from nose.tools import eq_
-
+# 0 - 1
+# 1 - 1
+# 2 - 1
+# 3 - 2
+# 4 - 2
+# 5 - 3
+# 6 - 4
+# 7 - 5
+# 8 - 6
+# 9 - 8
+# 10 - 10
 def test_answer():
-    pass
+    # define expected input / output values
+    test_data = {
+        0 : 0, 1 : 0, 2 : 0, 3 : 1, 4 : 1, 5 : 2,
+        6 : 3, 7 : 4, 8 : 5, 9 : 7, 10 : 9, 11 : 11
+    }
+
+    for key, expected in test_data.items():
+        eq_(expected, answer(key))
+
 
